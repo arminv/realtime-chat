032de1ad53f9324fe0b6a92323592a5159fd085c
arminv <arminv@users.noreply.github.com>
2020-08-23 02:49:02 -0400

    Progress (at 20:24)

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,8 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from '@apollo/client';
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'http://localhost:4000/',
   cache: new InMemoryCache(),
 });
+
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: '.5em',
+                border: '2px solid #e5e6ea',
+                borderRadius: 25,
+                textAlign: 'center',
+                fontSize: '18pt',
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? '#58bf56' : '#e5e6ea',
+              color: user === messageUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+  return JSON.stringify(data);
+};
+
+const Chat = () => {
+  const [state, stateSet] = useState({
+    user: 'Armin',
+    content: '',
+  });
+
+  return (
+    <Container>
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <FormInput
+            label='User'
+            value={state.user}
+            onChange={(e) => stateSet({ ...state, user: e.target.value })}
+          />
+        </Col>
+        <Col xs={8}>
+          <FormInput
+            label='Content'
+            value={state.content}
+            onChange={(e) => stateSet({ ...state, content: e.target.value })}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Chat />
+  </ApolloProvider>
+);
